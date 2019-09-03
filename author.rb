7f5da8dabab735c6f478aa3e2fd70a12683357c3
jgibbel <jamesgibbel@gmail.com>
2019-09-03 17:29:59 -0400

    working

--- author.rb
+++ author.rb
@@ -1,1 +1,31 @@
 
+class Author 
+    @@all = []
+    attr_reader :name
+    def initialize(name)
+        @name = name 
+        @@all << self 
+    end 
+
+    def self.all 
+        @@all
+    end
+
+    def books 
+        Book.all.select {|book| book.author == self}
+    end 
+
+    def write_book(title, word_count)
+        Book.new(self, title, word_count)
+    end 
+
+    def total_words 
+        books.map {|book| book.word_count}
+        .inject(:+)
+    end 
+
+    def self.most_words 
+        Author.all.max_by {|author| author.total_words}
+    end 
+end
+
--- console.rb
+++ console.rb
@@ -2,6 +2,18 @@ require 'pry'
 require_relative './book'
 require_relative './author'
 
+
+a1 = Author.new("A")
+a2 = Author.new("B")
+a3 = Author.new("C")
+
+a1.write_book("Book", 25000)
+a1.write_book("Book"*2, 50000)
+a2.write_book("Book"*5, 2500000)
+
+
+
 binding.pry
+0
 
 
